2a27b259ddeea2ae4ead0b11089699621924ab8a
soronto <soronto3603@gmail.com>
2023-06-25 18:49:54 +0900

    feat: CORS Origin 수정 #5

--- src/lambda.ts
+++ src/lambda.ts
@@ -10,6 +10,7 @@ async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
+      'http://localhost:5173',
       'https://moviemangae-front-git-develop-mandoong.vercel.app',
       'https://fullbloommovies.com',
       'https://api.fullbloommovies.com',
